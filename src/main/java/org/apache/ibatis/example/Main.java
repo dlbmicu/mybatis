76ec26c283703fef10352b86deac0bafeffe2174
dlbm <zh773907029@163.com>
2022-03-10 00:18:33 +0800

    commit

---
+++ src/main/java/org/apache/ibatis/example/Main.java
@@ -0,0 +1,28 @@
+package org.apache.ibatis.example;
+
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+//docker run -itd --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root  mysql
+//DROP DATABASE IF EXISTS MYBATIS;
+//CREATE DATABASE MYBATIS;
+//CREATE TABLE MYBATIS.USER (
+//        ID INT PRIMARY KEY AUTO_INCREMENT,
+//        NAME VARCHAR ( 255 ),
+//        AGE INT,
+//        KEY IDX_NAME ( NAME )
+//        );
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String resource = "config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSessionFactory.openSession();
+
+    }
+}
